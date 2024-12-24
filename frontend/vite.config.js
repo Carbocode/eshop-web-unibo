0d4a2aafccfdf598982c671499a15039a82134c5
Carbocode <noreply@prisco.me>
2024-12-24 22:19:19 +0100

    changed project structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
+  resolve:{
+    alias:{
+      '@global':path.resolve(__dirname, 'src/global.css'),
+      '@assets':path.resolve(__dirname, 'src/assets'),
+    }
+  },
+
   server: {
     port: 3000, // Porta per lo sviluppo
+    open: true
   },
+
   build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        login: path.resolve(__dirname, 'src/pages/login/index.html'),
+        register:path.resolve(__dirname, 'src/pages/register/index.html'),
+        toolkit:path.resolve(__dirname, 'src/pages/toolkit/index.html')
+      }
+    },
     outDir: 'dist', // Cartella per i file di produzione
+    assetsDir: 'assets'
   },
 });
