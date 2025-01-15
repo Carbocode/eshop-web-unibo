cdb8908e250cd950e70b6090b8a7683e69da1ba0
Carbocode <noreply@prisco.me>
2025-01-15 18:24:50 +0100

    fixed sql

--- backend/db/elprimerofootballer.sql
+++ backend/db/elprimerofootballer.sql
@@ -1,16 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Creato il: Gen 15, 2025 alle 18:02
--- Versione del server: 10.4.32-MariaDB
--- Versione PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
